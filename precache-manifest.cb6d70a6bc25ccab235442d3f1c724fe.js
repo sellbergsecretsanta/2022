52db473ad7d134f55344a7f6032ac95fcf7d3bd8
Erik Götesson <erik_gotesson@hotmail.com>
2022-10-29 16:25:31 +0200

    Updates

---
+++ precache-manifest.cb6d70a6bc25ccab235442d3f1c724fe.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af6ad543f8345c24714555c485942814",
+    "url": "/2022/index.html"
+  },
+  {
+    "revision": "2a02cc8352f3f729a5a5",
+    "url": "/2022/static/css/2.eabe93d2.chunk.css"
+  },
+  {
+    "revision": "55fb00a86ae6eaf681ef",
+    "url": "/2022/static/css/main.8fb0c4b0.chunk.css"
+  },
+  {
+    "revision": "2a02cc8352f3f729a5a5",
+    "url": "/2022/static/js/2.944cdf06.chunk.js"
+  },
+  {
+    "revision": "55fb00a86ae6eaf681ef",
+    "url": "/2022/static/js/main.902e79a7.chunk.js"
+  },
+  {
+    "revision": "911b8c6f2679755daeac",
+    "url": "/2022/static/js/runtime~main.71241e45.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/2022/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/2022/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/2022/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/2022/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/2022/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
